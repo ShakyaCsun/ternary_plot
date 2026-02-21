b7f709bbe1b02239b3c08554b0754abde1d0c0f4
Srishan Shakya <csunshakya@gmail.com>
2026-02-21 18:20:14 +0545

    refactor: value_list_notifier + pub upgrade

--- lib/src/models/value_list_notifier.dart
+++ lib/src/models/value_list_notifier.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/foundation.dart';
 
-/// Similar to [ValueNotifier] but for List and uses value equality and
-/// immutability for the List.
+/// Similar to [ValueNotifier] but for List and uses value equality.
 class ValueListNotifier<T> extends ChangeNotifier
     implements ValueListenable<List<T>> {
-  ValueListNotifier(List<T> value) : _value = List.unmodifiable(value);
+  ValueListNotifier(List<T> value) : _value = value;
 
   @override
   List<T> get value => _value;
   List<T> _value;
   set value(List<T> newValue) {
-    if (listEquals(_value, newValue)) {
+    if (_equals(_value, newValue)) {
       return;
     }
     _value = newValue;
@@ -19,4 +18,14 @@ class ValueListNotifier<T> extends ChangeNotifier
 
   @override
   String toString() => '${describeIdentity(this)}($value)';
+
+  static bool _equals<T>(List<T> list1, List<T> list2) {
+    if (identical(list1, list2)) return true;
+    final length = list1.length;
+    if (length != list2.length) return false;
+    for (var i = 0; i < length; i++) {
+      if (list1[i] != list2[i]) return false;
+    }
+    return true;
+  }
 }
